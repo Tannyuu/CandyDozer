523a03487d7858bf1f49f0a92bdad83eec23be2d
hogehoge <hogehoge@gmail.com>
2022-01-28 13:51:56 +0900

    p178まで作成

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    public float ampulitube;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.localPosition;
+        //transformがいきなりでてきたらscriptがアタッチされてるゲームオブジェクトを指す
+        //localPositionはオフセット量(親要素との差)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = ampulitube * Mathf.Sin(Time.time * speed);//Mathfは数学系のクラス
+
+        transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
