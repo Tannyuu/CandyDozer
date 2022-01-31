62d83dda1810fd5855e4f14d45b44d67e610ef3e
hogehoge <hogehoge@gmail.com>
2022-01-31 14:00:28 +0900

    p198まで作成

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Candy")//いきなりGameObjectだとこのスクリプトがアタッチされてるゲームオブジェクト
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject[] candyPrefabs;
+    public Transform candyParentTransform;//親子関係を結んで動きを連動させたりする GameObject型だとcandy.transform.parent = candyParent.transformになる GetComponentが必要になる
+    public float shotForce;
+    public float shotTorque;
+    public float baseWidth;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shot();  //Downを消すとボタン押しっぱなしで連射できる
+    }
+
+    GameObject SampleCandy() //GameObjectの戻り値を返す型
+    {
+        int index = Random.Range(0, candyPrefabs.Length);//0～3まで
+        return candyPrefabs[index];
+    }
+
+    Vector3 GetInstantiatePosition()
+    {
+        float x = baseWidth * (Input.mousePosition.x/*クリックしたxの座標*/ / Screen.width) - (baseWidth / 2);//画面の〇%の場所にシューターを向ける
+        return transform.position + new Vector3(x, 0, 0);
+    }
+
+    public void Shot()
+    {
+        GameObject candy = (GameObject)Instantiate(
+            SampleCandy(),
+            GetInstantiatePosition(),
+            Quaternion.identity//.Euler(0,0,0)でもいい　三次元空間の中の回転　position(0,0,0)でオブジェクトを作りたいときの表記
+                               //Euler(0,0,0);
+             );
+
+        candy.transform.parent = candyParentTransform;//親になるオブジェクトを設定する際には親になるGameObjectのtransformを代入する
+
+        Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+        candyRigidBody.AddForce(transform.forward * shotForce);//加える力
+        candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));//回転力
+    }
+}
